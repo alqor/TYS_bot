6648b7e05834f7ab8eb291d9663cfa4cd4505137
alqor <a.kylivnyk@gmail.com>
2021-11-14 20:34:57 +0200

    correct_app_name

--- parameters.py
+++ parameters.py
@@ -12,7 +12,7 @@ import os
 WEBHOOK_PORT = int(os.environ.get('PORT', 5000)) # 
 API_TOKEN =  os.environ.get('TYS_BOT_TOKEN')
 
-WEBHOOK_HOST = 'leoneed.herokuapp.com' # this is the name of heriku aap
+WEBHOOK_HOST = 'takeyourseats.herokuapp.com' # this is the name of heriku aap
 WEBHOOK_LISTEN = '0.0.0.0'  
 
 WEBHOOK_URL_BASE = f"https://{WEBHOOK_HOST}:{WEBHOOK_PORT}"
